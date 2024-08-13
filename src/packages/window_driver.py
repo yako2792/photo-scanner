c2b80a3f72a08e6fba7fb59bf7e1ec9e0bc9d6da
Alex <halo82331@gmail.com>
2024-08-12 21:27:13 -0600

    4-quadrant layout added and separated in methods

--- src/packages/window_driver.py
+++ src/packages/window_driver.py
@@ -14,6 +14,7 @@ class window_driver(QMainWindow):
 
         self.setup_window()
         self.setup_layout()
+        
 
     def setup_window(self, geometry = (800,600), title = "Unnamed"):
         """
@@ -22,29 +23,79 @@ class window_driver(QMainWindow):
         :param title: Title of window ("Untitled" by default).
         """
         self.setWindowTitle(title)
-        self.setGeometry(100,100, geometry[0], geometry[1])
+        self.setFixedSize(geometry[0], geometry[1])
+        self.setStyleSheet("background-color: #d6d0d4")
+
+        # Horizontal layout as main container
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+        self.main_layout = QHBoxLayout(self.central_widget)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
 
     def setup_layout(self):
         """
         Create initial layout content in window.
         """
-        # Horizontal layout as main container
-        self.central_widget = QWidget(self)
-        self.setCentralWidget(self.central_widget)
-        self.main_layout = QHBoxLayout(self.central_widget)
-
-        # Creation of left container (sliders)
-        self.left_column = QVBoxLayout()
-        self.left_column.setAlignment(Qt.AlignTop | Qt.AlignLeft)
-        self.left_column.addWidget(QLabel("Header in LEFT column"))
-        self.main_layout.addLayout(self.left_column)
-
-        # Creation of right container (camera and buttons)
-        self.right_column = QVBoxLayout()
-        self.right_column.setAlignment(Qt.AlignTop | Qt.AlignLeft)
-        self.right_column.addWidget(QLabel("Header in RIGHT column"))
-        self.main_layout.addLayout(self.right_column)
+        self.setup_left_column()
+        self.setup_right_column()
     
+    def setup_left_column(self):
+        """
+        Create left column in layout.
+        """
+        # Left column
+        self.left_column = QVBoxLayout()
+        self.main_layout.addLayout(self.left_column)
+        
+        ## Left upper box
+        self.left_top_box = QWidget()
+        self.left_top_box.setFixedSize(400,300)
+        # self.left_top_box.setStyleSheet("background-color: lightblue;")
+        
+        self.left_top_layout = QVBoxLayout(self.left_top_box)
+        self.left_top_layout.addWidget(QLabel("Left top box"))
+        self.left_top_layout.setContentsMargins(50, 0, 50, 0)
+        self.left_column.addWidget(self.left_top_box)
+
+        ## Left bottom box
+        self.left_bottom_box = QWidget()
+        self.left_bottom_box.setFixedSize(400,300)
+        # self.left_bottom_box.setStyleSheet("background-color: blue;")
+        
+        self.left_bottom_layout = QVBoxLayout(self.left_bottom_box)
+        self.left_bottom_layout.addWidget(QLabel("Left bottom box"))
+        self.left_bottom_layout.setContentsMargins(50, 0, 50, 0)
+        self.left_column.addWidget(self.left_bottom_box)
+
+    def setup_right_column(self):
+        """
+        Create right column in layout.
+        """
+        # Right column
+        self.right_column = QVBoxLayout()
+        self.main_layout.addLayout(self.right_column)
+
+        ## Right top box
+        self.right_top_box = QWidget()
+        self.right_top_box.setFixedSize(400,300)
+        self.right_top_box.setStyleSheet("background-color: black;")
+        
+        self.right_top_layout = QVBoxLayout(self.right_top_box)
+        self.label = QLabel("Right top box")
+        self.label.setStyleSheet("color: #ffffff")
+        self.right_top_layout.addWidget(self.label)
+        self.right_column.addWidget(self.right_top_box)
+
+        ## Right bottom box
+        self.right_bottom_box = QWidget()
+        self.right_bottom_box.setFixedSize(400,300)
+        # self.right_bottom_box.setStyleSheet("background-color: lightblue;")
+        
+        self.right_bottom_layout = QVBoxLayout(self.right_bottom_box)
+        self.right_bottom_layout.addWidget(QLabel("Right bottom box"))
+        self.right_bottom_layout.setContentsMargins(50, 0, 50, 0)
+        self.right_column.addWidget(self.right_bottom_box)
+
     def display_window(self):
         """
         Show app window.
